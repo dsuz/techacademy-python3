7a56c89d4060147adff94657dee73461dbcccaef
Daisuke Suzuki <bboydaisuke@gmail.com>
2018-07-28 13:52:50 +0900

    lesson7 の課題を更新した

--- lesson9-7-kuku.py
+++ lesson9-7-kuku.py
@@ -8,7 +8,23 @@ for v in range(1, 10):
 
 # pattern 1
 # 九九のリストを作成する
-table = [[(x * y) for y in range(1, 10)] for x in range(1, 10)]  # Lesson9 - 7. 二次元リスト でのリスト内包表記による二次元リストの作成例を参照
+table = [[(x * y) for y in range(1, 10)] for x in range(1, 10)]  # Lesson9 [7. 二次元リスト] でのリスト内包表記による二次元リストの作成例を参照
+
+# 九九の表を表示する
+for i in range(len(table)):
+    for j in range(len(table[i])):
+        print(f'{table[i][j]:3d}', end='')
+    print('')  # 改行
+
+# pattern 2
+# 九九のリストを作成する
+table = []
+# Lesson9 [7. 二次元リスト] での for_list_2d.py を参照
+for x in range(1, 10):
+    row = []
+    for y in range(1, 10):
+        row.append(x * y)
+    table.append(row)
 
 # 九九の表を表示する
 for i in range(len(table)):
