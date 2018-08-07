c9d8e3af3f6389de6eb12bab4f42331c7ffab245
Daisuke Suzuki <bboydaisuke@gmail.com>
2018-08-08 01:48:41 +0900

    ループの書き方がおかしいので修正した

--- exercise/lesson9-7-kuku.py
+++ exercise/lesson9-7-kuku.py
@@ -2,18 +2,18 @@
 # 九九の表を表示する
 for v in range(1, 10):
     for h in range(1, 10):
-        print(f"{(v * h):3d}", end="")
+        print(f'{(v * h):3d}', end='')
 
-    print("")
+    print('')
 
 # pattern 1
 # 九九のリストを作成する
 table = [[(x * y) for y in range(1, 10)] for x in range(1, 10)]  # Lesson9 [7. 二次元リスト] でのリスト内包表記による二次元リストの作成例を参照
 
 # 九九の表を表示する
-for i in range(len(table)):
-    for j in range(len(table[i])):
-        print(f'{table[i][j]:3d}', end='')
+for row in table:
+    for cell in row:
+        print(f'{cell:3d}', end='')
     print('')  # 改行
 
 # pattern 2
@@ -27,7 +27,7 @@ for x in range(1, 10):
     table.append(row)
 
 # 九九の表を表示する
-for i in range(len(table)):
-    for j in range(len(table[i])):
-        print(f'{table[i][j]:3d}', end='')
+for row in table:
+    for cell in row:
+        print(f'{cell:3d}', end='')
     print('')  # 改行
