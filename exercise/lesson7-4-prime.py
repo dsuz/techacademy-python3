114ea3c2f81e8a459fe94f5023bebc587c999bea
Daisuke Suzuki <bboydaisuke@gmail.com>
2018-08-08 23:14:35 +0900

    リストはまだ習っていなかったのでやめた

--- exercise/lesson7-4-prime.py
+++ exercise/lesson7-4-prime.py
@@ -1,19 +1,18 @@
-# pattern 1: 課題の仕様/実行結果に沿ったやり方
+# pattern 1: 課題の仕様の条件をそのまま実装するやり方
+# ただし、100 までなら問題ないが、このやり方では素数を判定する範囲が大きくなると正しい結果を得られない
 print('pattern 1')
-prime_list = []
-for n in range(1, 101):
-    if n == 2:
-        print(n)
-        prime_list.append(n)
-    if n > 2:
-        for p in prime_list:
-            if n % p == 0:
-                break
-        else:
-            print(n)
-            prime_list.append(n)
+for i in range(2, 101):
+    if i != 2 and i % 2 == 0:
+        continue
+    if i != 3 and i % 3 == 0:
+        continue
+    if i != 5 and i % 5 == 0:
+        continue
+    if i != 7 and i % 7 == 0:
+        continue
+    print(i)
 
-# pattern 2: 1 の判定を省略して短く書く
+# pattern 2: Python の特徴に沿って短く書くやり方。ただし、計算には無駄が多い。
 print('pattern 2')
 for n in range(2, 101):
     for p in range(2, n):
@@ -23,6 +22,7 @@ for n in range(2, 101):
         print(n)
 
 # pattern 3: 奇数のみに対して判定処理を行う
+# 計算の回数が少なくて済む
 print('pattern 3')
 for n in range(1, 101):
     if n == 2:  # 2は自明な素数として処理する
