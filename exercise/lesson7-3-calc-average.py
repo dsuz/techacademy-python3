5bdc6b72e69a702f801013c0eef03a7f461ef994
Daisuke Suzuki <bboydaisuke@gmail.com>
2018-08-08 22:50:01 +0900

    無駄があるので改良した

--- exercise/lesson7-3-calc-average.py
+++ exercise/lesson7-3-calc-average.py
@@ -1,12 +1,14 @@
-score_list = []
+counter = 0  # 人数のカウンター
+score_sum = 0  # 点数の合計
 while True:
     score = int(input('点数を入力してください：'))
     if score < 0:
         break
-    score_list.append(score)
+    counter += 1
+    score_sum += score
 
-if score_list:
-    score_average = sum(score_list) / len(score_list)
-    print(len(score_list), '人のテストの平均点は', score_average, '点です')
+if counter > 0:  # 一人も入力されていない時は処理しない
+    score_average = score_sum / counter  # 平均値
+    print(counter, '人のテストの平均点は', score_average, '点です')
 else:
     print('集計できません')
