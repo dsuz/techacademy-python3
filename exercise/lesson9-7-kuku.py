a28e532ca9b9a9f03e45d7996f6d69b8ad32944b
Daisuke Suzuki <bboydaisuke@gmail.com>
2018-08-12 07:02:41 +0900

    ログ出力を追加した

--- exercise/lesson9-7-kuku.py
+++ exercise/lesson9-7-kuku.py
@@ -1,5 +1,6 @@
 # pattern 0
 # 九九の表を表示する
+print('pattern 0')
 for v in range(1, 10):
     for h in range(1, 10):
         print(f'{(v * h):3d}', end='')
@@ -8,6 +9,7 @@ for v in range(1, 10):
 
 # pattern 1
 # 九九のリストを作成する
+print('pattern 1')
 table = [[(x * y) for y in range(1, 10)] for x in range(1, 10)]  # Lesson9 [7. 二次元リスト] でのリスト内包表記による二次元リストの作成例を参照
 
 # 九九の表を表示する
@@ -20,6 +22,7 @@ for row in table:
 # 九九のリストを作成する
 table = []
 # Lesson9 [7. 二次元リスト] での for_list_2d.py を参照
+print('pattern 2')
 for x in range(1, 10):
     row = []
     for y in range(1, 10):
