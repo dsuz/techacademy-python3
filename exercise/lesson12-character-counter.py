77b565ee764fd88a27d6c35b1d7b3949bfa1a2a4
Daisuke Suzuki <bboydaisuke@gmail.com>
2018-08-13 23:16:13 +0900

    コードの最適化

--- exercise/lesson12-character-counter.py
+++ exercise/lesson12-character-counter.py
@@ -1,11 +1,13 @@
-word_list = []
+word_list = []  # 単語リスト
 char_dic = {chr(i + 97): 0 for i in range(26)} # 文字 (a-z) の辞書、97 は 'a' の文字コード
 
 # 英単語の入力を受け付ける
 while True:
-    buf = input('英単語を入力してください：')
+    buf = input('英単語を入力してください：').lower()  # 念の為全部小文字にしておく
     if buf:
-        word_list.append(buf)
+        word_list.append(buf)  # 単語をリストに追加する
+        for c in buf:
+            char_dic[c] += 1  # 登場した文字をカウントアップする
     else:
         # 何も入力されずに Enter を押されたらループを抜けて次の処理をする
         break
@@ -14,13 +16,7 @@ if word_list:  # word_list に一つ以上の単語が含まれている時だ�
     word_list.sort()  # 並べ替える（ソート）
     print('入力した英単語：', word_list)
 
-    # word_list に格納された各単語に対して、文字数をカウントする
-    for word in word_list:
-        for char in word:
-            char_dic[char] += 1
-
     # char_dic に含まれている各文字に対して、カウントされた値を出力する
-    for char in char_dic:  # この char は数行上の char とはスコープが違い、異なるものであることに注意
-        count = char_dic[char]
-        if count > 0:  # 辞書の値が 1 以上の時だけ出力する
-            print(f'{char}が{count}個ありました')
+    for k, v in char_dic.items():  # k, v はそれぞれ key, value の短縮形で、文字とカウントされた値を表す
+        if v > 0:  # 辞書の値が 1 以上の時だけ出力する
+            print(f'{k}が{v}個ありました')
