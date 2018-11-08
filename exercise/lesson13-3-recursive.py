b891874e0dcfa64399cd7ac8fe9e387d78c2ce33
Daisuke Suzuki <bboydaisuke@gmail.com>
2018-11-08 10:47:25 +0900

    修正

--- exercise/lesson13-3-recursive.py
+++ exercise/lesson13-3-recursive.py
@@ -1,12 +1,12 @@
 # from numbers import fibonacci    # ディレクトリ構造に依存せず、同じディレクトリのモジュールを読み込むやり方
-from exercise import numbers  # ディレクトリ構造に依存するので要注意
+from exercise import lesson13_3_numbers   # ディレクトリ構造に依存するので要注意
 
 while True:
     try:
         n = int(input('何番目の数値を求めますか？：'))
 
         if n > 0:
-            print(numbers.fibonacci(n))
+            print(lesson13_3_numbers.fibonacci(n))
         else:
             print('1以上の数を指定してください')
     except ValueError:
--- exercise/lesson7-5-exception.py
+++ exercise/lesson7-5-exception.py
@@ -2,10 +2,13 @@ try:
     a = int(input("割られる数を入力してください："))
     b = int(input("割る数を入力してください："))
     c = a / b
-    print(f"{a} ÷ {b} = {c}")
 except ValueError:
     print('エラー：数値を入力してください')
 except ZeroDivisionError:
     print('エラー：0で割り算しないでください')
-
-print("処理を終了します")
+except:
+    print("エラー：何らかのエラーが発生しました")
+else:
+    print(f"{a} ÷ {b} = {c}")
+finally:
+    print("処理を終了します")
